f333525f8e262e6315eb4f2c024d0590860dcc6c
eason <ycwang@free.com>
2021-01-05 02:32:10 -0800

    leetcode practice 06 complete

--- leetcode/Normal_Practice_06/main.c
+++ leetcode/Normal_Practice_06/main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-// 2(n - rows -1) * i + rows   i = 0, 1, 2, 3 ......
+// if i != 0 and i != (numRows-1)
+// 2*(numRows-1)*k+i        k = 0, 2, 4, 6...... even
+// 2*(numRows-1)*(k+1)-i    k = 1, 3, 5, 7...... odd
 char * convert(char * s, int numRows){
     int s_size = strlen(s);
     printf("input string len: %d\n\n\n", s_size);
@@ -12,26 +14,39 @@ char * convert(char * s, int numRows){
     char* z_buf_start;
     char* z_buf = malloc(s_size + 1);
     z_buf_start = z_buf;
-    int jump_gap = 0;
+
+    if (s_size == 1 || numRows == 1){
+    	memcpy(z_buf, s, s_size+1);
+    	return z_buf;
+    }
 
     for(z_rows = 0; z_rows < numRows; z_rows++){
-        jump_gap = 2*(numRows - z_rows - 1);
-        
-        if (jump_gap == 0){
-        	jump_gap = 2*(numRows - 1);
-        }
-    
-        for(z_items = 0, z_buf_pointer = 0; ; z_items++){
+        for(z_items = 0, z_buf_pointer = z_rows; ; z_items++){
+        	printf("buffer pointer: %d\n", z_buf_pointer);
+			if (z_buf_pointer >= s_size){
+				printf("pointer over limit\n");
+				break;
+			}
+			printf("Append character: %c\n\n\n", *(s + z_buf_pointer));
+			*z_buf_start = *(s + z_buf_pointer);
+			z_buf_start++;
 
-            z_buf_pointer = jump_gap * z_items + z_rows;
-            printf("buffer pointer: %d\n", z_buf_pointer);
-            if (z_buf_pointer > s_size){
-            	printf("pointer over limit\n");
-                break;
-            }
-            printf("Append character: %c\n\n\n", *(s + z_buf_pointer));
-            *z_buf_start = *(s + z_buf_pointer);
-            z_buf_start++;
+        	if (z_rows == 0 || z_rows == numRows-1){
+        		z_buf_pointer += 2*(numRows-1);
+        	}
+
+        	else if(z_items%2 == 0){
+        		z_buf_pointer += 2*(numRows-1) - 2*z_rows;
+        	}
+
+        	else if(z_items%2 == 1){
+        		z_buf_pointer += 2*z_rows;
+        	}
+
+        	else{
+        		printf("Trigger error");
+        		while(1);
+        	}
         }
         printf("break rows: %d loop\n\n\n", z_rows);
     }
@@ -40,10 +55,10 @@ char * convert(char * s, int numRows){
     return z_buf;
 }
 
-char* input_string = "PAYPALISHIRING";
-int input_numRows = 4;
+char* input_string = /*"PAYPALISHIRING";*/"A";
+int input_numRows = 1;
 
-char* output_string = "PINALSIGYAHRPI";
+char* output_string = /*"PINALSIGYAHRPI";*/"A";
 int
 main (int argc, char *argv[])
 {
