c5e5637d920db36a7bdae33a879505688133918d
eason <ycwang@free.com>
2021-01-26 02:37:01 -0800

    [Data Struct][Practice] parenthesis match

---
+++ data_struct/stack/parenthesis_match/main.c
@@ -0,0 +1,47 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 25, 2021
+ *      Author: eason
+ */
+
+#include "LinearStack.h"
+#include "public.h"
+
+static linear_stack stack;
+
+int main(){
+	char match_string[] = "{}{()[[]()";
+	uint32_t num = 0;
+	char c_pop;
+
+	LinearStack_Init(&stack);
+
+	while(match_string[num]){
+		char c = match_string[num];
+		if(c == '{' || c == '(' || c == '['){
+			// push the character to stack
+			assert(!LinearStack_Push(&stack, &c));
+		}
+
+		if(c == '}' || c == ')' || c == ']'){
+			assert(!LinearStack_Pop(&stack, &c_pop));
+
+
+			if((c_pop == '{' && c == '}') || (c_pop == '[' && c == ']') || (c_pop == '(' && c == ')')){
+				LOG("In locate: %d, %c character compare success", num, c);
+			}else{
+				LOG("In locate: %d, %c character compare failed", num, c);
+				assert(0);
+			}
+		}
+		num++;
+	}
+
+	while(LinearStack_Empty(&stack)){
+		LinearStack_Pop(&stack, &c_pop);
+		LOG("[Error] residue character: %c", c_pop);
+	}
+
+	return 0;
+}
